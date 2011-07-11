9afc8b13ce3a1638de844896a92d8c1ce3c07731
shoma <shoma.h4a+github@gmail.com>
2011-07-11 23:03:28 +0900

    update for pypi

--- scalalike/actors/__init__.py
+++ scalalike/actors/__init__.py
@@ -1,1 +1,13 @@
 #-*- coding:utf-8 -*-
+'''
+scala like actor interface for python.
+'''
+
+from actor import ok, error, NoReceive, Actor, current, \
+    sender, reply, receive, start_child
+
+
+__version__ = '0.1.0'
+__author__ = 'Shoma Hosaka'
+__license__ ='LGPL'
+
--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@
 
 import setuptools
 
-version = '0.0.1'
+from scalalike.actors import __version__, __author__, __doc__, __license__
+
+
 
 setuptools.setup(
     name='scalalike.actors',
+    version=__version__,
+    author=__author__,
+    author_email='shoma.h4a+pypi@gmail.com',
+    license=__license__,
+    url='https://github.com/shomah4a/actors',
+    description='scala like actor interface for python.',
+    long_description=__doc__,
     packages=['scalalike.actors'],
     install_requires=[
+        'setuptools',
         ],
     include_dirs=['test', 'test/*.py'],
     namespace_packages=['scalalike'],
@@ -15,4 +25,14 @@ setuptools.setup(
         ('test', ['test/*.py']),
         ],
     include=['test/*.py'],
-    version=version)
+    classifiers='''
+Programming Language :: Python
+Development Status :: 2 - Pre-Alpha
+License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Utilities
+'''.strip().splitlines())
+
